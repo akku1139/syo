52e77a923ff85d6eb7f57e401c11792739e186ad
-akku- <151517253+akku1139@users.noreply.github.com>
2025-02-27 05:59:39 +0000

    remove rehype-pretty-code

--- src/source/markdown.ts
+++ src/source/markdown.ts
@@ -2,7 +2,7 @@ import { unified } from "unified"
 import markdown from "remark-parse"
 import remark2rehype from "remark-rehype"
 import rehypeTwoslash from 'rehype-twoslash'
-import rehypePrettyCode from "rehype-pretty-code"
+// import rehypePrettyCode from "rehype-pretty-code"
 import rehypeStringify from "rehype-stringify"
 import type { SourceProcessor } from "../types.ts"
 
@@ -10,7 +10,7 @@ const processor = unified()
   .use(markdown)
   .use(remark2rehype)
   .use(rehypeTwoslash)
-  .use(rehypePrettyCode)
+  // .use(rehypePrettyCode)
   .use(rehypeStringify)
 
 export const markdownProcessor: SourceProcessor = async source => (await processor.process(source)).toString()
