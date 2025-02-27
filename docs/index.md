922c06d1c53eb015290c1e268055aa2a47ebd57a
-akku- <151517253+akku1139@users.noreply.github.com>
2025-02-27 05:57:03 +0000

    Code block

--- docs/index.md
+++ docs/index.md
@@ -3,3 +3,11 @@
 Cool and Easy documentation site generator
 
 ## This is example
+
+```ts twoslash
+import { defineConfig } from "../src/config.ts"
+
+export default defineConfig({
+  title: "Test"
+})
+```
--- src/source/markdown.ts
+++ src/source/markdown.ts
@@ -1,12 +1,16 @@
 import { unified } from "unified"
 import markdown from "remark-parse"
 import remark2rehype from "remark-rehype"
-import html from "rehype-stringify"
+import rehypeTwoslash from 'rehype-twoslash'
+import rehypePrettyCode from "rehype-pretty-code"
+import rehypeStringify from "rehype-stringify"
 import type { SourceProcessor } from "../types.ts"
 
 const processor = unified()
   .use(markdown)
   .use(remark2rehype)
-  .use(html)
+  .use(rehypeTwoslash)
+  .use(rehypePrettyCode)
+  .use(rehypeStringify)
 
 export const markdownProcessor: SourceProcessor = async source => (await processor.process(source)).toString()
