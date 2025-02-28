c6d60c3be4c316fba2a61208f3b846c13d62eb27
-akku- <151517253+akku1139@users.noreply.github.com>
2025-02-28 03:25:20 +0000

    marked

--- src/source/markdown.ts
+++ src/source/markdown.ts
@@ -1,16 +1,6 @@
-import { unified } from "unified"
-import markdown from "remark-parse"
-import remark2rehype from "remark-rehype"
-import rehypeTwoslash from "rehype-twoslash"
-// import rehypePrettyCode from "rehype-pretty-code"
-import rehypeStringify from "rehype-stringify"
 import type { SourceProcessor } from "../types.ts"
+import { Marked } from "marked"
 
-const processor = unified()
-  .use(markdown)
-  .use(remark2rehype)
-  .use(rehypeTwoslash)
-  // .use(rehypePrettyCode)
-  .use(rehypeStringify)
+const marked = new Marked()
 
-export const markdownProcessor: SourceProcessor = async source => (await processor.process(source)).toString()
+export const markdownProcessor: SourceProcessor = marked.parse
