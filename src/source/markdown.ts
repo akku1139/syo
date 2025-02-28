9b9a5b04542c9614dd593509c9d2db8f0df92df8
-akku- <151517253+akku1139@users.noreply.github.com>
2025-02-28 15:07:08 +0900

    shiki?

--- src/source/markdown.ts
+++ src/source/markdown.ts
@@ -1,15 +1,27 @@
+import { codeToHtml } from "shiki"
+import { transformerTwoslash } from "@shikijs/twoslash"
 import type { SourceProcessor } from "../types.ts"
 import { Marked } from "marked"
 
 export const markdownProcessor: SourceProcessor = async (source: string) => {
   let title: string = ""
   const marked = new Marked({
+    async: true,
     gfm: true,
-    walkTokens(token) {
+    async walkTokens(token) {
       if(token.type === "heading" && token.depth === 1) {
         title = token.text
       }
-    }
+      if(token.type === "code") {
+        token.text = codeToHtml(token.text, {
+          lang: token.lang,
+          theme: "vitesse-dark",
+          transformers: [
+            transformerTwoslash(),
+          ]
+        })
+      }
+    },
   })
   const content = await marked.parse(source)
 
