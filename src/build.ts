543bb8eddab4811446b4d11dfd1a08cf6c6a5d1a
-akku- <151517253+akku1139@users.noreply.github.com>
2025-03-15 00:37:59 +0900

    better cli

--- src/build.ts
+++ src/build.ts
@@ -3,8 +3,11 @@ import * as fsSync from "node:fs"
 import { type Config } from "./config.ts"
 import { build as farmBuild } from "@farmfe/core"
 import { markdownPlugin } from "./farm-plugins/markdown.ts"
+import * as process from "node:process"
 
 export const build = async (config: Config): Promise<void> => {
+  process.env.NODE_ENV = "production"
+
   const srcDir = config.srcDir ?? "docs"
   // const srcs = fs.glob(`${srcDir}/**/*.md`)
   const srcs = fsSync.globSync(`${srcDir}/**/*.md`)
