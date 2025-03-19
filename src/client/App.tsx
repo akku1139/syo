c8086963852cd95a0e448a72224b407dba6853d3
-akku- <151517253+akku1139@users.noreply.github.com>
2025-03-19 22:51:49 +0900

    avoid bug

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,6 +1,10 @@
 import { Router } from "@solidjs/router"
 import { createSignal } from "solid-js"
-const routes = (await import("syo:routes")).default
+import routes from "syo:routes"
+
+// https://github.com/farm-fe/farm/issues/2125
+// const routes = (await import("syo:routes")).default
+
 const [routesSignal, _setRoutes] = createSignal(routes)
 
 export default () => <Router>{
