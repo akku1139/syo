00427291a25675f3a0c06c49c3e453dc3ad184da
-akku- <151517253+akku1139@users.noreply.github.com>
2025-03-19 23:30:33 +0900

    fix routing

--- docs/syo.config.js
+++ docs/syo.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from "../src/node/config.ts"
 
 export default defineConfig({
   title: "Syo",
-  basePath: "./",
+  basePath: "/syo/",
 })
--- src/node/farm-plugins/routing.ts
+++ src/node/farm-plugins/routing.ts
@@ -1,13 +1,13 @@
 import type { FarmJSPlugin } from "../types.ts"
 import { p } from "../utils/path.ts"
 
-export const routingPlugin = ({ routes }: Parameters<FarmJSPlugin>[0]): [string, object] => [
+export const routingPlugin = ({ config, routes }: Parameters<FarmJSPlugin>[0]): [string, object] => [
   "@farmfe/plugin-virtual",
   {
     "syo:routes": `
       import { lazy } from "solid-js"
       export default [ ${routes.map(route => `{
-        path: ${JSON.stringify(route[0].replace(/\/?index$/, "/"))},
+        path: ${JSON.stringify((config.basePath ?? "/")+route[0].replace(/\/?index$/, "/"))},
         component: lazy(() => import("${p(route[1])}")),
         }`)
       } ]`
