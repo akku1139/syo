ddf7428b2de40a1490de5e3c9be3dd55b66d5e46
-akku- <151517253+akku1139@users.noreply.github.com>
2025-03-20 09:03:39 +0900

    change plugin order

--- src/node/build.ts
+++ src/node/build.ts
@@ -37,6 +37,12 @@ export const build = async (config: Config): Promise<void> => {
       routingPlugin({ config, routes }),
     ],
     vitePlugins: [
+      () => ({
+        vitePlugin: mdx({
+          jsxImportSource: "solid-js"
+        }),
+        filters: ["\\.md$", "\\.mdx$"]
+      }),
       // @farmfe/js-plugin-solid is deprecated
       // https://github.com/farm-fe/farm/issues/2124#issuecomment-2736695432
       () => ({
@@ -47,12 +53,6 @@ export const build = async (config: Config): Promise<void> => {
           extensions: [".md", ".mdx"],
         }),
         filters: ["\\.tsx$", "\\.jsx$", "\\.md$", "\\.mdx$"],
-      }),
-      () => ({
-        vitePlugin: mdx({
-          jsxImportSource: "solid-js"
-        }),
-        filters: ["\\.md$", "\\.mdx$"]
       }),
     ]
   })
