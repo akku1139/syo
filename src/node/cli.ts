191e403a6c583cc0818e06ad924d67a1632002de
-akku- <151517253+akku1139@users.noreply.github.com>
2025-03-20 11:26:09 +0900

    fix import path

--- src/node/cli.ts
+++ src/node/cli.ts
@@ -3,7 +3,7 @@ import * as v from "valibot"
 import { ConfigSchema } from "./config.ts"
 import { build } from "./build.ts"
 import { p } from "./utils/path.ts"
-import { dynamicImport } from "../raw/import.js"
+import { dynamicImport } from "../../src/raw/import.js"
 
 const configFilePath = p("syo.config.js")
 const config = v.parse( ConfigSchema,
